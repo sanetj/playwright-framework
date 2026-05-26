6e4e672ee9bbb45faaa5e0e5abe83f2bc4459cde
Sanket j <sanketjadhavar@gmail.com>
2026-05-26 22:33:53 +0530

    phase9.3: add admin workflow risk rule

--- src/intelligence/workflow-analysis/rules/workflow-risk-rules.ts
+++ src/intelligence/workflow-analysis/rules/workflow-risk-rules.ts
@@ -1,4 +1,5 @@
 import { WorkflowDiscoveryResult } from '../../workflow-discovery/discovery-engine';
+import { WorkflowEntityCategory } from '../../workflow-models/workflow-classification';
 
 export interface WorkflowRiskRule {
   id: string;
@@ -7,4 +8,13 @@ export interface WorkflowRiskRule {
   ): boolean;
 }
 
-export const WORKFLOW_RISK_RULES: WorkflowRiskRule[] = [];
+const ADMIN_ENTITY_RULE: WorkflowRiskRule = {
+  id: 'admin_entity_rule',
+  evaluate(discoveryResult: WorkflowDiscoveryResult): boolean {
+    return discoveryResult.entities.some(e => e.category === WorkflowEntityCategory.ADMIN);
+  }
+};
+
+export const WORKFLOW_RISK_RULES: WorkflowRiskRule[] = [
+  ADMIN_ENTITY_RULE
+];
